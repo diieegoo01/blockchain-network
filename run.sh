9831a8837f618913d3037cc4f8c33d59755090e8
Diego Reyes Delgado <diegoreyes@Diegos-MacBook-Pro-2.local>
2018-07-17 19:33:02 -0400

    changes run and stop bash

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+docker stack deploy -c hyperledger-zookeeper.yaml hyperledger-zk
+docker stack deploy -c hyperledger-kafka.yaml hyperledger-kafka
 docker stack deploy -c hyperledger-orderer.yaml hyperledger-orderer
 docker stack deploy -c hyperledger-couchdb.yaml hyperledger-couchdb
 docker stack deploy -c hyperledger-peer.yaml hyperledger-peer
--- stop.sh
+++ stop.sh
@@ -1,6 +1,7 @@
 # Exit on first error, print all commands.
 set -ev
-
+docker stack rm hyperledger-zk
+docker stack rm hyperledger-kafka
 docker stack rm hyperledger-orderer
 docker stack rm hyperledger-couchdb
 docker stack rm hyperledger-peer
