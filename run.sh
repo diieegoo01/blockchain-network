afe3d499e6ad01b6f9bd353536bda42baa4bad84
Diego <diego@peer1.c.strong-keyword-114421.internal>
2018-07-17 06:15:54 +0000

    Changes certificates

--- run.sh
+++ run.sh
@@ -4,13 +4,4 @@ docker stack deploy -c hyperledger-peer.yaml hyperledger-peer
 docker stack deploy -c hyperledger-ca.yaml hyperledger-ca
 docker stack deploy -c hyperledger-cli.yaml hyperledger-cli
 
-# wait for Hyperledger Fabric to start
-# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
-export FABRIC_START_TIMEOUT=20
-#echo ${FABRIC_START_TIMEOUT}
-sleep ${FABRIC_START_TIMEOUT}
-
-# Create the channel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.lorachain.io/msp" peer0.org1.lorachain.io peer channel create -o orderer.lorachain.io:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
-# Join peer0.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.lorachain.io/msp" peer0.org1.lorachain.io peer channel join -b mychannel.block
+echo "Listo compareee"
