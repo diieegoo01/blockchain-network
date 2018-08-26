570da8e27887054c47d7ebfa6f635ce7509c9118
diieegoo01 <diiego_reyes@live.cl>
2018-08-26 17:46:50 -0300

    Checkout preview

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ docker stack deploy -c hyperledger-orderer.yaml hyperledger-orderer
 docker stack deploy -c hyperledger-couchdb.yaml hyperledger-couchdb
 docker stack deploy -c hyperledger-peer.yaml hyperledger-peer
 docker stack deploy -c hyperledger-ca.yaml hyperledger-ca
+docker stack deploy -c hyperledger-cli.yaml hyperledger-cli
 
 echo "Listo compareee"
