d962ebf4cd01528387aa5d6cefc731bf87d39469
diieegoo01 <diiego_reyes@live.cl>
2018-08-26 14:06:24 -0300

    change configuration TLS Containers

--- run.sh
+++ run.sh
@@ -4,6 +4,5 @@ docker stack deploy -c hyperledger-orderer.yaml hyperledger-orderer
 docker stack deploy -c hyperledger-couchdb.yaml hyperledger-couchdb
 docker stack deploy -c hyperledger-peer.yaml hyperledger-peer
 docker stack deploy -c hyperledger-ca.yaml hyperledger-ca
-docker stack deploy -c hyperledger-cli.yaml hyperledger-cli
 
 echo "Listo compareee"
